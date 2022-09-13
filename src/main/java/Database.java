758451ef3014073196c1f513e295604e7e11f6df
Asgertor <asge1114@stud.kea.dk>
2022-09-13 15:51:27 +0200

    SuperHero_v2

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -4,11 +4,15 @@ public class Database {
 
     private ArrayList<SuperHero> superHeroes = new ArrayList<>();
 
-    public SuperHero getSuperhero(int index){
+    public SuperHero getSuperhero(int index) {
         return superHeroes.get(index);
     }
-    public void addSuperHero(String name, String superHeroName, String race, String superPower, double powerLevel, int yearOfOrigin ){
+
+    public void addSuperHero(String name, String superHeroName, String race, String superPower, double powerLevel, int yearOfOrigin) {
         superHeroes.add(new SuperHero(name, superHeroName, race, superPower, powerLevel, yearOfOrigin));
     }
+    public ArrayList<SuperHero> getAllSuperheroes(){
+        return superHeroes;
     }
 
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,27 +4,40 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.printf("MENU\n1. new superhero\n9. quit program\n");
-        int userinput = sc.nextInt();
+
         Database db = new Database();
         while (true) {
+            System.out.println("""
+                
+                Menu options:
+                1. New superhero
+                2. View current superheroes
+                9. quit program
+                """);
+            int userInput = sc.nextInt();
+            sc.nextLine();
 
-            if (userinput == 1) {
+            if (userInput == 1) {
                 System.out.printf("\nCreate new superhero:\n");
                 System.out.print("Real name:");
-                String realName = sc.next();
+                String realName = sc.nextLine();
                 System.out.print("Superhero name:");
-                String superHeroName = sc.next();
+                String superHeroName = sc.nextLine();
                 System.out.print("Race:");
-                String race = sc.next();
+                String race = sc.nextLine();
                 System.out.print("Superpower:");
-                String superPower = sc.next();
+                String superPower = sc.nextLine();
                 System.out.print("Powerlevel:");
                 double powerlevel = sc.nextDouble();
                 System.out.print("Year of origin:");
                 int yearOfOrigin = sc.nextInt();
                 db.addSuperHero(realName, superHeroName, race, superPower, powerlevel, yearOfOrigin);
-                System.out.println(db.getSuperhero(0));
+            }
+            else if (userInput == 2) {
+                for (SuperHero superHero: db.getAllSuperheroes()) {
+                    System.out.println(superHero);
+                }
+                
             } else {
                 System.exit(0);
             }
